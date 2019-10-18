592861340eb893a2958c6521a4d8aeb2d40717ea
BryanNilsen <bryannilsen@gmail.com>
2019-10-18 13:47:47 -0500

    user can purchase sesame and plant in plowed field

--- src/Models/Farm.cs
+++ src/Models/Farm.cs
@@ -6,39 +6,48 @@ using Trestlebridge.Models.Facilities;
 
 namespace Trestlebridge.Models
 {
-    public class Farm
+  public class Farm
+  {
+    public List<GrazingField> GrazingFields { get; } = new List<GrazingField>();
+    public List<PlowedField> PlowedFields { get; } = new List<PlowedField>();
+
+    /*
+        This method must specify the correct product interface of the
+        resource being purchased.
+     */
+    public void PurchaseResource<T>(IResource resource, int index)
     {
-        public List<GrazingField> GrazingFields { get; } = new List<GrazingField>();
-
-        /*
-            This method must specify the correct product interface of the
-            resource being purchased.
-         */
-        public void PurchaseResource<T> (IResource resource, int index)
-        {
-            Console.WriteLine(typeof(T).ToString());
-            switch (typeof(T).ToString())
-            {
-                case "Cow":
-                    GrazingFields[index].AddResource((IGrazing)resource);
-                    break;
-                default:
-                    break;
-            }
-        }
-
-        public void AddGrazingField (GrazingField field)
-        {
-            GrazingFields.Add(field);
-        }
-
-        public override string ToString()
-        {
-            StringBuilder report = new StringBuilder();
-
-            GrazingFields.ForEach(gf => report.Append(gf));
-
-            return report.ToString();
-        }
+      Console.WriteLine(typeof(T).ToString());
+      switch (typeof(T).ToString())
+      {
+        case "Cow":
+          GrazingFields[index].AddResource((IGrazing)resource);
+          break;
+        case "Sesame":
+          PlowedFields[index].AddResource((ISeedProducing)resource);
+          break;
+        default:
+          break;
+      }
     }
+
+    public void AddGrazingField(GrazingField field)
+    {
+      GrazingFields.Add(field);
+    }
+    public void AddPlowedField(PlowedField field)
+    {
+      PlowedFields.Add(field);
+    }
+
+    public override string ToString()
+    {
+      StringBuilder report = new StringBuilder();
+
+      GrazingFields.ForEach(gf => report.Append(gf));
+      PlowedFields.ForEach(pf => report.Append(pf));
+
+      return report.ToString();
+    }
+  }
 }
--- src/Models/Plants/Sesame.cs
+++ src/Models/Plants/Sesame.cs
@@ -3,17 +3,24 @@ using Trestlebridge.Interfaces;
 
 namespace Trestlebridge.Models.Plants
 {
-    public class Sesame : IResource, ISeedProducing
+  public class Sesame : IResource, ISeedProducing
+  {
+    private int _seedsProduced = 40;
+    public string Type { get; } = "Sesame";
+
+    public double HarvestSeed()
     {
-        private int _seedsProduced = 40;
-        public string Type { get; } = "Sesame";
-
-        public double Harvest () {
-            return _seedsProduced;
-        }
-
-        public override string ToString () {
-            return $"Sesame. Yum!";
-        }
+      return _seedsProduced;
     }
+
+    public double Seed()
+    {
+      return _seedsProduced;
+    }
+
+    public override string ToString()
+    {
+      return $"Sesame. Yum!";
+    }
+  }
 }
